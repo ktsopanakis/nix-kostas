1533b90a2c6812b285c3699573e90f3e8d15bea8
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2025-10-27 12:58:33 +0200

    update

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -6,9 +6,10 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+      [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        (import (builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz") { config = { }; pkgs = pkgs; })
+      ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
