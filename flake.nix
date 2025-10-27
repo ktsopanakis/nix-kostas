d7f805c798e38436895c36fc4f1217c39dc9e466
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2025-10-27 12:25:40 +0200

    update

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Omarchy-style NixOS + Home Manager setup (Hyprland, Waybar, flakes)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    # Make sure flakes are on even in bare shells
+    nixConfig = {
+      extra-experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    nixosConfigurations.draven = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/draven/configuration.nix
+
+        # Home Manager as a NixOS module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ktsop = import ./home/ktsop/home.nix;
+        }
+      ];
+    };
+  };
+}
