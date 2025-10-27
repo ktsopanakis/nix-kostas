a5bde2b24163a97b0b0f114b7ea13e7527cfa991
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2025-10-27 19:33:25 +0200

    Reset main to state of e72b844 (without rewriting history)

--- home/ktsop/home.nix
+++ home/ktsop/home.nix
@@ -1,11 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [
-    ./alacritty.nix
-    ./vscode.nix
-  ];
-
   home.username = "ktsop";
   home.homeDirectory = "/home/ktsop";
   home.stateVersion = "25.05";
@@ -60,7 +55,96 @@
     package = pkgs.firefox-wayland;
   };
 
+  # Alacritty terminal with configuration
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Italic";
+        };
+        size = 11.0;
+        offset = {
+          x = 0;
+          y = 1;
+        };
+      };
+      window = {
+        opacity = 0.95;
+        padding = {
+          x = 10;
+          y = 10;
+        };
+      };
+      colors = {
+        primary = {
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+        };
+        normal = {
+          black = "#45475a";
+          red = "#f38ba8";
+          green = "#a6e3a1";
+          yellow = "#f9e2af";
+          blue = "#89b4fa";
+          magenta = "#f5c2e7";
+          cyan = "#94e2d5";
+          white = "#bac2de";
+        };
+      };
+    };
+  };
 
+  # VS Code with extensions and settings
+  programs.vscode = {
+    enable = true;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # Language support
+        ms-python.python
+        ms-vscode.cpptools
+        rust-lang.rust-analyzer
+        golang.go
+        
+        # Nix support
+        bbenoist.nix
+        
+        # Git integration
+        eamodio.gitlens
+        
+        # Themes and UI
+        catppuccin.catppuccin-vsc
+        pkief.material-icon-theme
+        
+        # Productivity
+        ms-vscode.live-server
+        esbenp.prettier-vscode
+        bradlc.vscode-tailwindcss
+      ];
+      userSettings = {
+        "editor.fontSize" = 14;
+        "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'Droid Sans Mono', 'monospace'";
+        "editor.tabSize" = 2;
+        "editor.insertSpaces" = true;
+        "editor.minimap.enabled" = false;
+        "workbench.colorTheme" = "Catppuccin Mocha";
+        "workbench.iconTheme" = "material-icon-theme";
+        "terminal.integrated.fontSize" = 13;
+        "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font";
+        "git.enableSmartCommit" = true;
+        "git.confirmSync" = false;
+      };
+    };
+  };
 
   # Shell aliases for convenience
   home.shellAliases = {
